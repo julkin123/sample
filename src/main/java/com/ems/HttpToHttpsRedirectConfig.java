9c50d9695385c05fc28e9558bcb14887d68b93f2
julkin <julkingeorge8@gmail.com>
2024-12-18 18:26:15 +0100

    dvausscivaidschuaq

---
+++ src/main/java/com/ems/HttpToHttpsRedirectConfig.java
@@ -0,0 +1,24 @@
+package com.ems;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpToHttpsRedirectConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080); // Default HTTP port
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // Redirect to HTTPS port
+        return connector;
+    }
+}
---
+++ src/main/java/com/ems/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.ems;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+     OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://your-domain.com"))
+                .info(new Info().title("API").version("1.0"));
+    }
+}
+
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//@Configuration
+//public class SpringDocConfig implements WebMvcConfigurer
+//{
+//
+//
+//	 @Override
+//	    public void addCorsMappings(CorsRegistry registry) {
+//	        registry.addMapping("/**")
+//	                .allowedOrigins(
+//	                        "http://aware-empathy-production.up.railway.app",
+//	                        "http://localhost:8080",
+//	                        "http://aware-empathy-production.up.railway.app/swagger-ui/index.html"
+//	                )
+//	                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//	                .allowedHeaders("*")
+//	                ;
+//	    }
+//}
